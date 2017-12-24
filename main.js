7dd15cb8a09dbcf1f169ec5be9ed64a6431d6df0
Alex Bogomolov <alex-sert2015@mail.ru>
2017-12-24 21:28:43 +0300

    Hw2

--- main.js
+++ main.js
@@ -1,70 +1,27 @@
 const http = require("http");
 const fs = require("fs");
 
+let getTime = () => {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    return `${hours} : ${minutes} : ${seconds}`;
+};
+
+const NUMSEC = process.env.NUMSEC || 1000;
+const STOPTIME = process.env.STOPTIME || 4000;
 
 http.createServer(function(request, response) {
-        fs.readFile("./index.html" , function (err, data) { 
-            if (err) throw new Error("error");
-            response.end(data);   
-        })    
+        let setTime = setInterval(() => {
+            newTime = getTime();
+            console.log(newTime);
+            response.write(`${newTime}  \n`);
+        }, NUMSEC);
+
+        setTimeout(function() {
+            clearInterval(setTime);
+            console.log("стоп");
+            response.end();
+        }, STOPTIME);
     }).listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function startTimer() {
-//     return new Promise((res, rej) => {
-//         http
-//             .createServer(function(request, response) {
-//                 fs.readFile("./index.html", function(err, data) {
-//                     if (err) throw new Error("error");
-//                     response.end(data);
-//                     res(data);
-//                 });
-//             })
-//             .listen(3000);
-//     });
-// }
-
-// startTimer()
-//     .then(function(data) {
-//         let getTime = () => {
-//             let date = new Date();
-//             let hours = date.getHours();
-//             let minutes = date.getMinutes();
-//             let seconds = date.getSeconds();
-//             return `${hours} : ${minutes} : ${seconds}`;
-//         };
-//         let numSec = prompt("Введите интервал времени в сукундах", 2) * 1000;
-//         let stopTime = prompt("ВВедит срок работы таймера в секундах", 0) * 1000;
-//         let timer = document.querySelector(".timer");
-//         let newTime = document.createElement("h1");
-//         timer.appendChild(newTime);
-//         let setTime = setInterval(() => {
-//             newTime.innerHTML = getTime();
-//             timer.appendChild(newTime);
-//         }, numSec);
-
-//         setTimeout(function() {
-//             clearInterval(setTime);
-//             alert("стоп");
-//         }, stopTime);
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
